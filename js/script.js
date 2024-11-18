7068f361880ac77a2720700ddb1ac1b663c322ea
giuseppecasaburi <enzobiondi0000@libero.it>
2024-11-18 18:16:19 +0100

    feat: implementazione bonus

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 const listElm = document.getElementById("email-list");
+const btnElmReset = document.getElementById("btn-reset");
+const choiseElm = document.querySelector(".hidden")
 
+function emailGeneretor () {
+    listElm.innerHTML = "";
 
-for (let i = 0; i < 10; i++) {
-    axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
-        listElm.innerHTML += `
-            <li class="email">
-                <a href="">${resp.data.response} </a>
-            </li>
-            `
-})}
+    for (let i = 0; i < 10; i++) {
+        axios.get("https://flynn.boolean.careers/exercises/api/random/mail").then((resp) => {
+            listElm.innerHTML += `
+                <li class="email col-6 center">
+                    <a href="">${resp.data.response} </a>
+                </li>
+                `
+    })}
+}
 
+btnElmReset.addEventListener("click", function () {
+    emailGeneretor();
+    choiseElm.classList.remove("hidden");
+    choiseElm.classList.add("block")
 
+})
